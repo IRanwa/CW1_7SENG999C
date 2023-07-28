9dbfb955ad162ce5dfbc75b26a9c3c91055465bd
Imesh Ranawaka <imesh.20221787@iit.ac.lk>
2023-07-29 03:13:45 +0800

    loan screen development

--- CW1_7SENG999C/Controllers/LoanViewController.swift
+++ CW1_7SENG999C/Controllers/LoanViewController.swift
@@ -7,14 +7,49 @@
 
 import UIKit
 
-class LoanViewController: UIViewController {
+class LoanViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var presentTxtField: UITextField!
+    @IBOutlet weak var futureTxtField: UITextField!
+    @IBOutlet weak var interestTxtField: UITextField!
+    @IBOutlet weak var paymentTxtField: UITextField!
+    @IBOutlet weak var noPaymentPerYearTxtField: UITextField!
+    @IBOutlet weak var compoundPerYearTxtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Keyboard numbering
+        presentTxtField.keyboardType = .decimalPad
+        futureTxtField.keyboardType = .decimalPad
+        interestTxtField.keyboardType = .decimalPad
+        paymentTxtField.keyboardType = .decimalPad
+        noPaymentPerYearTxtField.keyboardType = .numberPad
+        compoundPerYearTxtField.keyboardType = .numberPad
+        
+        presentTxtField.delegate = self
+        futureTxtField.delegate = self
+        interestTxtField.delegate = self
+        paymentTxtField.delegate = self
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString txtVal: String) -> Bool {
+        if textField.keyboardType == .decimalPad {
+            if txtVal == "." {
+                if textField.text?.contains(".") ?? false {
+                    return false
+                }
+            }else if(txtVal != ""){
+                let splitByDecimal = textField.text?.split(separator: ".")
+                if let splitByDecimalList = splitByDecimal{
+                    if(splitByDecimalList.count > 1 && splitByDecimalList[1].count >= 2){
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
 
     /*
     // MARK: - Navigation
