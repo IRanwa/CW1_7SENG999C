878ba0cefe53989bd63d8159e4b35cd357173663
Imesh Ranawaka <imesh.20221787@iit.ac.lk>
2023-07-28 10:30:53 +0800

    Initial Commit

---
+++ CW1_7SENG999C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CW1_7SENG999C
+//
+//  Created by user235597 on 7/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
