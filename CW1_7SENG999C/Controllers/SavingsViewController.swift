a8fb2b516f1a402099cc5826c6519aac890fa87e
Imesh Ranawaka <imesh.20221787@iit.ac.lk>
2023-07-29 03:41:58 +0800

    ui developments

--- CW1_7SENG999C/Controllers/SavingsViewController.swift
+++ CW1_7SENG999C/Controllers/SavingsViewController.swift
@@ -7,14 +7,53 @@
 
 import UIKit
 
-class SavingsViewController: UIViewController {
+class SavingsViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var principalAmtTxtField: UITextField!
+    @IBOutlet weak var interestTxtField: UITextField!
+    @IBOutlet weak var paymentTxtField: UITextField!
+    @IBOutlet weak var compoundsPerYearTxtField: UITextField!
+    @IBOutlet weak var paymentsPerYearTxtfField: UITextField!
+    @IBOutlet weak var futureValueTxtField: UITextField!
+    @IBOutlet weak var noOfPaymentsTotalTxtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        principalAmtTxtField.keyboardType = .decimalPad
+        interestTxtField.keyboardType = .decimalPad
+        paymentTxtField.keyboardType = .decimalPad
+        compoundsPerYearTxtField.keyboardType = .numberPad
+        paymentsPerYearTxtfField.keyboardType = .numberPad
+        futureValueTxtField.keyboardType = .decimalPad
+        noOfPaymentsTotalTxtField.keyboardType = .numberPad
+        
+        principalAmtTxtField.delegate = self
+        interestTxtField.delegate = self
+        paymentTxtField.delegate = self
+        compoundsPerYearTxtField.delegate = self
+        paymentsPerYearTxtfField.delegate = self
+        futureValueTxtField.delegate = self
+        noOfPaymentsTotalTxtField.delegate = self
     }
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString txtVal: String) -> Bool {
+        if textField.keyboardType == .decimalPad {
+            if txtVal == "." {
+                if textField.text?.contains(".") ?? false {
+                    return false
+                }
+            }else if(txtVal != ""){
+                let splitByDecimal = textField.text?.split(separator: ".")
+                if let splitByDecimalList = splitByDecimal{
+                    if(splitByDecimalList.count > 1 && splitByDecimalList[1].count >= 2){
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
 
     /*
     // MARK: - Navigation
